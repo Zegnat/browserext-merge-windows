fbb171458b90e292b9ac2575b4f3133bb351b2ba
Martijn van der Ven <martijn@vanderven.se>
2020-03-28 17:34:12 +0100

    Introduce option for ignoring minimised windows

--- preferences.js
+++ preferences.js
@@ -1,20 +1,24 @@
+function update(preferences) {
+  for (const preference in preferences) {
+    const value = 'object' === typeof preferences[preference] ? preferences[preference].newValue : preferences[preference]
+    if (Number.isInteger(value)) {
+      document.querySelector('[name="' + preference + '"][value="' + value + '"]').checked = true
+    } else {
+      document.querySelector('[name="' + preference + '"]').checked = value
+    }
+  }
+}
+
 browser.storage.local.get({
   context_menu_location: 0,
-  merge_insertion: 0
-}).then(preferences => {
-  for (const preference in preferences) {
-    document.querySelector('[name="' + preference + '"][value="' + preferences[preference] + '"]').checked = true
-  }
-})
+  merge_insertion: 0,
+  ignore_minimised: false
+}).then(update)
 
 document.body.addEventListener('change', ({ target }) => {
   const save = {}
-  save[target.name] = Number.parseInt(target.value)
+  save[target.name] = 'checkbox' === target.type ? target.checked : Number.parseInt(target.value)
   browser.storage.local.set(save)
 })
 
-browser.storage.onChanged.addListener(changes => {
-  for (const change in changes) {
-    document.querySelector('[name="' + change + '"][value="' + changes[change].newValue + '"]').checked = true
-  }
-})
+browser.storage.onChanged.addListener(update)
